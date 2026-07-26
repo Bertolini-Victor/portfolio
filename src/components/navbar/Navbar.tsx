5ab008db59531295ab52eee6bbd574eb1a7641e8
Victor Bertolini <118063703+Bertolini-Victor@users.noreply.github.com>
2026-07-26 20:11:46 -0300

    add the profile photo and change the size

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -111,7 +111,7 @@ export const Navbar: React.FC = ({}) => {
 		<nav className="navbar">
 			<div className="navbar-profile">
 				<img
-					src="https://via.placeholder.com/150"
+					src="./public/foto_perfil.jpeg"
 					alt="Victor Bertolini Agaras"
 					className="navbar-avatar"
 				/>
